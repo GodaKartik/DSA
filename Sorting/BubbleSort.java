2e0dc80102fa7baa4b1e11bbe1bf578470f77cd4
Kartik <90519209+GodaKartik@users.noreply.github.com>
2023-07-28 17:00:45 +0530

    Create BubbleSort.java

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,17 @@
+class BubbleSort{
+    BubbleSort(int array[]){
+        this.sort(array);
+    }
+    int[] sort(int array[]){
+        for (int i = 0; i < array.length - 1; i++){
+            for (int j = 1; j < array.length; j++){
+                if (array[j-1] > array[j]) {
+					int temp = array[j-1];
+					array[j-1] = array[j];
+					array[j] = temp;
+				}
+            }
+        }
+        return array;
+    }
+}
