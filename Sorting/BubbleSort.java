c4152caad7f2f5e08fb5d763d39d315626c3ae1a
Kartik <90519209+GodaKartik@users.noreply.github.com>
2023-08-06 01:20:04 +0530

    Update BubbleSort.java

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -6,12 +6,14 @@ class BubbleSort{
         for (int i = 0; i < array.length - 1; i++){
             for (int j = 1; j < array.length; j++){
                 if (array[j-1] > array[j]) {
-					int temp = array[j-1];
-					array[j-1] = array[j];
-					array[j] = temp;
-				}
+			int temp = array[j-1];
+			array[j-1] = array[j];
+			array[j] = temp;
+		}
             }
         }
         return array;
     }
 }
+
+// change type of 'array' (line 2 and line 5) and 'temp' to whatever type of numeric data being used
